cf108cd9ef96a73d2554bef9a35d2839ea48c83a
ongyuxing <78018704+ongyuxing@users.noreply.github.com>
2021-07-14 14:35:39 +0800

    Update package config

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,34 @@
+import babel from '@rollup/plugin-babel'
 import typescript from '@rollup/plugin-typescript'
+import eslint from '@rollup/plugin-eslint'
+
+const extensions = ['.js', '.jsx', 'ts', 'tsx', '.mjs']
 
 export default [
   {
     input: 'src/index.ts',
-    output: {
-      dir: 'dist',
-      sourcemap: true,
-      format: 'cjs',
-    },
-    watch: {
-      include: 'src/**',
-      exclude: 'node_modules/**',
-    },
+    output: [
+      {
+        dir: 'dist',
+        sourcemap: true,
+        format: 'cjs',
+      },
+      {
+        file: 'dist/index.mjs',
+        sourcemap: true,
+        format: 'es',
+      },
+    ],
     plugins: [
+      eslint(),
+      babel({
+        extensions,
+        babelHelpers: 'bundled',
+      }),
       typescript({
         declaration: true,
         declarationDir: 'dist',
       }),
     ],
-  },
-  {
-    input: 'src/index.ts',
-    output: {
-      file: 'dist/index.mjs',
-      sourcemap: true,
-      format: 'esm',
-    },
-    watch: {
-      include: 'src/**',
-      exclude: 'node_modules/**',
-    },
-    plugins: [typescript()],
   },
 ]
